3ab77066a2514d8c25fbd0fb74be8cdff2c56eaa
Hosein Arabnia <hossein.arabnia@gmail.com>
2025-11-16 23:10:02 +0330

    fix or add some feauture in day25-1 main.py

--- day25-1/main.py
+++ day25-1/main.py
@@ -10,16 +10,14 @@ screen.bgpic("photos/blank_states_img.gif")
 
 def duplicate_name(answer):
     for state in answers:
-        if state.name == answer.lower():
+        if state.lower() == answer.lower():
             return True
 
 def check_answer(answer):
     if data[data.state.str.lower() == answer].empty:
-        # messagebox.showinfo("Wrong name!", f"there isn't state called {answer}")
-        screen.textinput("Wrong!", "There is no state with that name.")
+        messagebox.showinfo("Wrong name!", f"there isn't state called {answer}")
         return False
     elif duplicate_name(answer):
-        print(duplicate_name(answer))
         messagebox.showinfo("Duplicate name!", f"{answer} is Duplicate")
         return False
     else:
@@ -29,24 +27,40 @@ def fill_map(answer):
     state_name = t.Turtle()
     state_name.color("black")
     state_name.penup()
-    state_name.name = answer.lower()
+    # state_name.name = answer.lower()
     state_name.hideturtle()
     x_cor = int(data[data.state == answer].x.iloc[0])
     y_cor = int(data[data.state == answer].y.iloc[0])
     state_name.goto(x_cor, y_cor)
     style = ('Courier', 10, 'italic')
     state_name.write(answer, font=style, align='center')
-    answers.append(state_name)
+    answers.append(answer)
 
 
 while len(answers) != 50:
     if len(answers) == 0:
         answer = screen.textinput(f"{len(answers)}/50 States Correct", "Gives the firs state name!").lower()
-        if check_answer(answer):
+        if answer == "exit":
+            break
+        elif check_answer(answer):
             fill_map(data[data.state.str.lower() == answer].state.iloc[0])
     else:
         answer = screen.textinput(f"{len(answers)}/50 States Correct", "Whats another state name?").lower()
-        if check_answer(answer):
-            fill_map(answer)
-# screen.textinput("input","input")
-screen.exitonclick()
+        if answer == "exit":
+            break
+        elif check_answer(answer):
+            fill_map(data[data.state.str.lower() == answer].state.iloc[0])
+
+# you must learn!
+learn_state = []
+all_states = data.state.to_list()
+print(all_states)
+for state in all_states:
+    if state not in answers:
+        learn_state.append(state)
+
+learn_state = pd.DataFrame(learn_state)
+learn_state.to_csv("data/learn_states.csv")
+
+
+# screen.exitonclick()
