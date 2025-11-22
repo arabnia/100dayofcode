443f280020ec8aae736fe5758201897491a9565f
Hosein Arabnia <hossein.arabnia@gmail.com>
2025-11-22 21:39:14 +0330

    add tkinter sample file

--- day27/main.py
+++ day27/main.py
@@ -1,9 +1,19 @@
-import tkinter as tk
-window = tk.Tk()
+from tkinter import *
+window = Tk()
 window.title("GUI example")
 window.minsize(600, 300)
-my_label = tk.Label(font=("B Nazanin", 24, "bold"), text="زندگی هنوز زیباست.")
-my_label.pack(side="right")
+header = Label(window, text="First text")
+header.pack()
+def submit_button():
+    in_func_var = box_test.get()
+    box_test.delete(0, END)
+    header.config(text=in_func_var)
+
+button = Button(window, text="button", command=submit_button)
+button.pack()
+box_test = Entry()
+box_test.pack()
+
 
 
 window.mainloop()
