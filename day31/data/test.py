7024d2e52fa9a6f63a7e8c828a843477a4b1eb42
Hosein Arabnia <hossein.arabnia@gmail.com>
2025-11-29 22:50:00 +0330

    day31 chalange6: auto flip flashcard

--- day31/data/test.py
+++ day31/data/test.py
@@ -2,6 +2,8 @@ import pandas as pd
 import random
 data = pd.read_csv("translated_most_freq_word.csv")
 def random_word():
-    return random.choice(data["word"])
+    return data.sample(n=1, random_state=random.seed(1))
 
-print(random_word())
+print(random_word()["word"].iloc[0])
+
+
