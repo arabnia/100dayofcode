0bcaeb259e928bad9a035ebed6c6cb45aede8fc3
Hosein Arabnia <hossein.arabnia@gmail.com>
2026-01-03 20:56:00 +0330

    day45 final chalange

--- day45/main.py
+++ day45/main.py
@@ -1,29 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-target_site = requests.get("https://news.ycombinator.com/news")
-soup = BeautifulSoup(target_site.content, "html.parser")
-items = soup.find_all("span", class_="titleline")
-title_link = [item.find("a")["href"] for item in items]
-title_text = [item.find("a").get_text() for item in items]
+url = "https://www.empireonline.com/movies/features/best-movies-of-all-time-us/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/122.0 Safari/537.36",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+}
+target_site = requests.get(url, headers=headers, timeout=10)
 
-soup_votes = soup.find_all("span", class_="score")
-title_score = [int(soup_vote.get_text().split()[0]) for soup_vote in soup_votes]
+target_soup = BeautifulSoup(target_site.content, "html.parser")
 
-max_score_index = title_score.index(max(title_score))
-print(title_text[max_score_index])
-print(title_link[max_score_index])
-
-print(len(title_text))
-print(len(title_link))
-print(len(title_score))
-data = pd.DataFrame(
-    {
-     "title": title_text,
-     "link": title_link,
-     "score": title_score
-    }
-)
-data.to_csv("data.csv", index=False)
-title_links = []
-title_vote = []
+items = target_soup.select("h2 strong")
+movie_title = [ item.get_text() for item in items]
+with open("movie_title.txt", "w") as f:
+    f.write("\n".join(movie_title))
